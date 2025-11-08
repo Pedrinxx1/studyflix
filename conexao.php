0dc1199bb098511ac7d0b9791cd8e779959ec6f8
Pedrinxx1 <pehe19544@gmail.com>
2025-11-08 16:03:02 -0300

    eff

--- conexao.php
+++ conexao.php
@@ -1,14 +1,15 @@
 <?php
-$host = "aws-1-us-east-1.pooler.supabase.com";
-$dbname = "postgres";
-$user = "postgres.hmaqoyajkalregfmdhdo";
-$password = "AnnaLuisa12";
-$port = "5432";
+// Dados da conexão Render PostgreSQL
+$host = 'dpg-d46m637gi27c73au64t0-a';  // servidor
+$port = '5432';                        // porta padrão do PostgreSQL
+$dbname = 'studyflix_db';              // nome do banco
+$user = 'studyflix_db_user';           // usuário
+$password = 'jAvxXoAmej1Zz1mlgUmLWbs4Sg9lAj1Z'; // senha
 
 try {
     $conn = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "✅ Conexão bem-sucedida com o Supabase!";
+    echo "✅ Conexão bem-sucedida com o Render PostgreSQL!";
 } catch (PDOException $e) {
     echo "❌ Erro na conexão: " . $e->getMessage();
 }
