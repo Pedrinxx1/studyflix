5e24393ae85b5e280e927d15b2b13189e0eed4b9
Pedrinxx1 <pehe19544@gmail.com>
2025-11-08 17:48:18 -0300

    hg

--- conexao.php
+++ conexao.php
@@ -1,17 +1,16 @@
 <?php
-// Lê as variáveis de ambiente configuradas no Render
-$host = getenv('dpg-d47ph0k9c44c73cbi1dg-a');
-$port = getenv('5432');
-$dbname = getenv('studyflix_db_qurq');
-$user = getenv('studyflix_user');
-$password = getenv('C7RDk7jynwGOQqr78NGhBDB7a2QCapvo');
+// Conexão com banco de dados Render (usando interno)
+$host = 'dpg-d47ph0k9c44c73cbi1dg-a';
+$port = '5432';
+$dbname = 'studyflix_db_qurq';
+$user = 'studyflix_user';
+$password = 'C7RDk7jynwGOQqr78NGhBDB7a2QCapvo';
 
 try {
     $conn = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "✅ Conexão bem-sucedida!";
+    echo "✅ Conexão bem-sucedida com o Render PostgreSQL!";
 } catch (PDOException $e) {
     echo "❌ Erro na conexão: " . $e->getMessage();
-    exit;
 }
 ?>
